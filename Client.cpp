ea622746286d1c7559e0427b1b4cbb2b41e36396
NatsuEGL <54336789+NatsuEGL@users.noreply.github.com>
2024-09-25 10:42:53 +0100

    adding server and commands
    after doing some job without uploading finally uploading the project with the server done and some commands (still need to be fixed a little bit).

---
+++ Client.cpp
@@ -0,0 +1,57 @@
+#include "Client.hpp"
+
+Client::Client()
+{
+	this->nickname = "";
+	this->username = "";
+	this->fd = -1;
+	this->isOperator= false;
+	this->registered = false;
+	this->buffer = "";
+	this->ipadd = "";
+	this->logedin = false;
+}
+Client::Client(std::string nickname, std::string username, int fd) :fd(fd), nickname(nickname), username(username){}
+Client::~Client(){}
+
+//---------------//Getters
+bool Client::GetInviteChannel(std::string &ChName){
+	for (size_t i = 0; i < this->ChannelsInvite.size(); i++){
+		if (this->ChannelsInvite[i] == ChName)
+			return true;
+	}
+	return false;
+}
+int Client::GetFd(){return this->fd;}
+bool Client::getRegistered(){return registered;}
+std::string Client::GetNickName(){return this->nickname;}
+std::string Client::GetUserName(){return this->username;}
+std::string Client::getBuffer(){return buffer;}
+std::string Client::getIpAdd(){return ipadd;}
+std::string Client::getHostname(){
+	std::string hostname = this->GetNickName() + "!" + this->GetUserName();
+	return hostname;
+}
+//---------------//Getters
+//---------------//Setters
+void Client::SetFd(int fd){this->fd = fd;}
+void Client::SetNickname(std::string& nickName){this->nickname = nickName;}
+void Client::setLogedin(bool value){this->logedin = value;}
+void Client::SetUsername(std::string& username){this->username = username;}
+void Client::setBuffer(std::string recived){buffer += recived;}
+void Client::setRegistered(bool value){registered = value;}
+void Client::setIpAdd(std::string ipadd){this->ipadd = ipadd;}
+//---------------//Setters
+//---------------//Methods
+void Client::clearBuffer(){buffer.clear();}
+bool Client::GetLogedIn(){return this->logedin;}
+void Client::AddChannelInvite(std::string &chname){
+	ChannelsInvite.push_back(chname);
+}
+void Client::RmChannelInvite(std::string &chname){
+	for (size_t i = 0; i < this->ChannelsInvite.size(); i++){
+		if (this->ChannelsInvite[i] == chname)
+			{this->ChannelsInvite.erase(this->ChannelsInvite.begin() + i); return;}
+	}
+}
+
---
+++ Makefile
@@ -0,0 +1,26 @@
+NAME = ircserv
+CC = c++
+FLAGS = -Wall -Wextra -Werror -std=c++98
+
+SRCS = main.cpp Server.cpp Client.cpp Auth.cpp Channel.cpp Join.cpp
+
+
+OBJS = $(SRCS:.cpp=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(CC) $(FLAGS) -o $(NAME) $(OBJS)
+
+%.o: %.cpp Server.hpp Client.hpp replies.hpp Channel.hpp
+	$(CC) $(FLAGS) -c $< -o $@
+
+clean:
+	@rm -f $(OBJS) 
+
+fclean: clean
+	@rm -f $(NAME) 
+
+re: fclean all
+
+.PHONY: all clean fclean re
